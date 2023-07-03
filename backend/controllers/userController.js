16c5853e52fcdf6802c1855c78d502ba139094f1
Kadir Akar <aakarkadir@gmail.com>
2023-07-03 21:40:38 +0300

    register - login - logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,25 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
 //@desc Auth user/set token
 //@route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Auth user",
-  });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@desc Register a new user
@@ -29,10 +42,11 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
-      name: user.name,
       email: user.email,
+      name: user.name,
     });
   } else {
     res.status(400);
@@ -44,6 +58,10 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/logout
 //@access Public
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
   res.status(200).json({
     message: "Logout user",
   });
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import Jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = Jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export default generateToken;
